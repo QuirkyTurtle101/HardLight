f9e1283cee5531a1d52ddee61d8c494832c73a36
RealMasterChief117 <officialtrollfacexxx@gmail.com>
2025-10-22 10:27:04 -0400

    fixes

--- Content.Client/Ghost/GhostSystem.cs
+++ Content.Client/Ghost/GhostSystem.cs
@@ -1,17 +1,10 @@
 using Content.Client.Movement.Systems;
-using Content.Client.UserInterface.Systems.Ghost.Widgets;
 using Content.Shared.Actions;
 using Content.Shared.Ghost;
-using Content.Shared.Mind;
-using Content.Shared.Popups;
 using Robust.Client.Console;
 using Robust.Client.GameObjects;
 using Robust.Client.Player;
-using Robust.Client.UserInterface;
-using Robust.Shared.GameStates;
-using Robust.Shared.Timing;
 using Robust.Shared.Player;
-using Content.Client._Corvax.Respawn; // Frontier
 
 namespace Content.Client.Ghost
 {
@@ -22,20 +15,7 @@ namespace Content.Client.Ghost
         [Dependency] private readonly SharedActionsSystem _actions = default!;
         [Dependency] private readonly PointLightSystem _pointLightSystem = default!;
         [Dependency] private readonly ContentEyeSystem _contentEye = default!;
-        // [Dependency] private readonly EyeSystem _eye = default!; // Frontier: suppress warnings
-        [Dependency] private readonly IUserInterfaceManager _uiManager = default!;
-        [Dependency] private readonly IGameTiming _gameTiming = default!;
-        [Dependency] private readonly RespawnSystem _respawn = default!;
-
-        public override void Update(float frameTime)
-        {
-            foreach (var ghost in EntityManager.EntityQuery<GhostComponent, MindComponent>(true))
-            {
-                var ui = _uiManager.GetActiveUIWidgetOrNull<GhostGui>();
-                if (ui != null && Player != null)
-                    ui.UpdateRespawn(_respawn.RespawnResetTime);
-            }
-        }
+        [Dependency] private readonly SpriteSystem _sprite = default!;
 
         public int AvailableGhostRoleCount { get; private set; }
 
@@ -56,7 +36,7 @@ namespace Content.Client.Ghost
                 var query = AllEntityQuery<GhostComponent, SpriteComponent>();
                 while (query.MoveNext(out var uid, out _, out var sprite))
                 {
-                    sprite.Visible = value || uid == _playerManager.LocalEntity;
+                    _sprite.SetVisible((uid, sprite), value || uid == _playerManager.LocalEntity);
                 }
             }
         }
@@ -93,7 +73,7 @@ namespace Content.Client.Ghost
         private void OnStartup(EntityUid uid, GhostComponent component, ComponentStartup args)
         {
             if (TryComp(uid, out SpriteComponent? sprite))
-                sprite.Visible = GhostVisibility || uid == _playerManager.LocalEntity;
+                _sprite.SetVisible((uid, sprite), GhostVisibility || uid == _playerManager.LocalEntity);
         }
 
         private void OnToggleLighting(EntityUid uid, EyeComponent component, ToggleLightingActionEvent args)
@@ -164,9 +144,6 @@ namespace Content.Client.Ghost
 
         private void OnGhostPlayerAttach(EntityUid uid, GhostComponent component, LocalPlayerAttachedEvent localPlayerAttachedEvent)
         {
-            if (uid != _playerManager.LocalPlayer?.ControlledEntity)
-                return;
-            component.TimeOfDeath = _gameTiming.CurTime;
             GhostVisibility = true;
             PlayerAttached?.Invoke(component);
         }
@@ -174,7 +151,7 @@ namespace Content.Client.Ghost
         private void OnGhostState(EntityUid uid, GhostComponent component, ref AfterAutoHandleStateEvent args)
         {
             if (TryComp<SpriteComponent>(uid, out var sprite))
-                sprite.LayerSetColor(0, component.Color);
+                _sprite.LayerSetColor((uid, sprite), 0, component.Color);
 
             if (uid != _playerManager.LocalEntity)
                 return;
