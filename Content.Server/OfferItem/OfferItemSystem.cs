13d35a6e06b6a7ab3458762276d56c341aa2bb27
VividPups <150621839+VividPups@users.noreply.github.com>
2025-10-22 18:29:09 -0500

    Update Content.Server/OfferItem/OfferItemSystem.cs
    Co-authored-by: TheRealMasterChief117 <officialtrollfacexxx@gmail.com>

--- Content.Server/OfferItem/OfferItemSystem.cs
+++ Content.Server/OfferItem/OfferItemSystem.cs
@@ -43,7 +43,7 @@ public sealed class OfferItemSystem : SharedOfferItemSystem
                 continue;
             }
 
-            _alertsSystem.ShowAlert(uid, AlertType.Offer);
+            _alertsSystem.ShowAlert(uid, offerItem.OfferAlert);
         }
     }
 
