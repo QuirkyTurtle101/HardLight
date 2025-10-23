5911d7ac399aa26f5ebd1843e2500b017b7dab62
VividPups <150621839+VividPups@users.noreply.github.com>
2025-10-23 18:33:27 -0500

    Update Content.Server/Alert/Click/AcceptingOffer.cs
    Co-authored-by: TheRealMasterChief117 <officialtrollfacexxx@gmail.com>

--- Content.Server/Alert/Click/AcceptingOffer.cs
+++ Content.Server/Alert/Click/AcceptingOffer.cs
@@ -8,17 +8,4 @@ namespace Content.Server.Alert.Click;
 /// <summary>
 /// Accepting the offer and receive item
 /// </summary>
-[UsedImplicitly]
-[DataDefinition]
-public sealed partial class AcceptOffer : IAlertClick
-{
-    public void AlertClicked(EntityUid player)
-    {
-        var entManager = IoCManager.Resolve<IEntityManager>();
-
-        if (entManager.TryGetComponent(player, out OfferItemComponent? offerItem))
-        {
-            entManager.System<OfferItemSystem>().Receive(player, offerItem);
-        }
-    }
-}
+public sealed partial class AcceptOfferAlertEvent : BaseAlertEvent;
